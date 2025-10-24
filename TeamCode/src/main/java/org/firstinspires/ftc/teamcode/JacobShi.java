7f5edab29af493e2655324b9a76d1564c732e7ae
Pirate1113 <156390222+Pirate1113@users.noreply.github.com>
2025-10-24 11:16:28 -0400

    Add JacobShi TeleOp and update dependencies
    Introduced a new TeleOp class JacobShi for shooter motor control with RPM telemetry and dashboard integration. Updated build.gradle and build.dependencies.gradle to add ARCore and FtcDashboard dependencies. Minor formatting changes in sample files.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JacobShi.java
@@ -0,0 +1,54 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+
+@TeleOp
+@Config
+public class JacobShi extends OpMode {
+
+    DcMotorEx shooter;
+
+    double power = 0.0;
+
+    boolean previousDpadUp;
+    boolean previousDpadDown;
+
+    double RPM;
+
+    FtcDashboard dashboard;
+
+    @Override
+    public void init() {
+        shooter = hardwareMap.get(DcMotorEx.class, "drive");
+
+        shooter.setDirection(DcMotor.Direction.REVERSE);
+        shooter.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        dashboard = FtcDashboard.getInstance();
+    }
+
+    public void loop() {
+        if (gamepad1.dpad_up && !previousDpadUp) {
+            power += 0.1;
+        } else if (gamepad1.dpad_down && !previousDpadDown) {
+            power -= 0.1;
+        }
+
+        shooter.setPower(power);
+
+        RPM = shooter.getVelocity()*60/28;
+
+        telemetry.addData("RPM", RPM);
+        telemetry.update();
+
+        telemetry = dashboard.getTelemetry();
+    }
+}
