8ab3e33ef6b7169d97ad910c64527889fe043bcf
adityatangella <adityatangella9@gmail.com>
2025-12-30 21:14:05 -0500

    newnewnewnewnewnew

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/BasicOpMode_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/BasicOpMode_Linear.java
@@ -59,8 +59,6 @@ import com.qualcomm.robotcore.util.Range;
 public class BasicOpMode_Linear extends LinearOpMode {
 
     private Spindexer spindexer;
-
-    // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor leftDrive = null;
     private DcMotor rightDrive = null;
@@ -70,9 +68,6 @@ public class BasicOpMode_Linear extends LinearOpMode {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
-        // Initialize the hardware variables. Note that the strings used here as parameters
-        // to 'get' must correspond to the names assigned during the robot configuration
-        // step (using the FTC Robot Controller app on the phone).
         leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
         rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
 
@@ -83,47 +78,45 @@ public class BasicOpMode_Linear extends LinearOpMode {
 
         spindexer = new Spindexer(leftServo, rightServo, ejectorServo, intakeColor);
 
-        // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
-        // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
-        // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
         leftDrive.setDirection(DcMotor.Direction.REVERSE);
         rightDrive.setDirection(DcMotor.Direction.FORWARD);
 
-        // Wait for the game to start (driver presses START)
         waitForStart();
         runtime.reset();
-
         spindexer.init();
 
-        // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
 
-            // Setup a variable for each drive wheel to save power level for telemetry
-            double leftPower;
-            double rightPower;
-
-            // Choose to drive using either Tank Mode, or POV Mode
-            // Comment out the method that's not used.  The default below is POV.
-
-            // POV Mode uses left stick to go forward, and right stick to turn.
-            // - This uses basic math to combine motions and is easier to drive straight.
             double drive = -gamepad1.left_stick_y;
             double turn  =  gamepad1.right_stick_x;
-            leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
-            rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+            double leftPower  = Range.clip(drive + turn, -1.0, 1.0);
+            double rightPower = Range.clip(drive - turn, -1.0, 1.0);
 
-            // Tank Mode uses one stick to control each wheel.
-            // - This requires no math, but it is hard to drive forward slowly and keep straight.
-            // leftPower  = -gamepad1.left_stick_y ;
-            // rightPower = -gamepad1.right_stick_y ;
 
-            // Send calculated power to wheels
             leftDrive.setPower(leftPower);
             rightDrive.setPower(rightPower);
 
-            // Show the elapsed game time and wheel power.
+            // spindexer controls
+            if (gamepad1.a) {
+                spindexer.intakeOne(telemetry);
+                sleep(250);
+            }
+            if (gamepad1.b) {
+                spindexer.ejectAllGreenThenPurple(telemetry);
+                sleep(250);
+            }
+            if (gamepad1.x) {
+                spindexer.rezeroHere(0);
+                sleep(250);
+            }
+
+            // telemetry
+            Spindexer.Ball[] slots = spindexer.getSlots();
+            telemetry.clearAll();
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+            telemetry.addData("IntakeIndex", spindexer.getIntakeIndex());
+            telemetry.addData("Slots", "%s | %s | %s", slots[0], slots[1], slots[2]);
             telemetry.update();
         }
     }
