cfbc3ee57636424a3f5849a9c41aad6bac3cac52
David Wang <73254509+ducc-coder@users.noreply.github.com>
2026-01-02 15:19:58 -0500

    support left and right intake power

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/Intake.java
@@ -12,30 +12,59 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 public class Intake extends OpMode {
 
     DcMotorEx intakeleft;
+    DcMotorEx intakeright;
 
-    double power = 0.0;
+    double powerLeft = 0.0;
+    double powerRight = 0.0;
 
     boolean previousDpadUp;
     boolean previousDpadDown;
+    boolean previousX;
+    boolean previousB;
 
 
     @Override
     public void init() {
         intakeleft = hardwareMap.get(DcMotorEx.class, "intakeleft");
+        intakeright = hardwareMap.get(DcMotorEx.class, "intakeright");
 
         intakeleft.setDirection(DcMotor.Direction.REVERSE);
         intakeleft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
+        intakeright.setDirection(DcMotor.Direction.FORWARD);
+        intakeright.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
     }
 
     public void loop() {
+        // Left intake control with dpad
         if (gamepad1.dpad_up && !previousDpadUp) {
-            power += 0.1;
+            powerLeft += 0.1;
         } else if (gamepad1.dpad_down && !previousDpadDown) {
-            power -= 0.1;
+            powerLeft -= 0.1;
         }
 
-        intakeleft.setPower(power);
+        // Right intake control with X (square) and B (circle)
+        if (gamepad1.x && !previousX) {
+            powerRight += 0.1;
+        } else if (gamepad1.b && !previousB) {
+            powerRight -= 0.1;
+        }
+
+        // Set motor powers
+        intakeleft.setPower(powerLeft);
+        intakeright.setPower(powerRight);
+
+        // Update previous button states
+        previousDpadUp = gamepad1.dpad_up;
+        previousDpadDown = gamepad1.dpad_down;
+        previousX = gamepad1.x;
+        previousB = gamepad1.b;
+
+        // Telemetry for debugging
+        telemetry.addData("Left Intake Power", powerLeft);
+        telemetry.addData("Right Intake Power", powerRight);
+        telemetry.update();
 
     }
 }
