4a8484e9ea0cfdc99bfb41599a295c83dbc21a18
Pirate1113 <156390222+Pirate1113@users.noreply.github.com>
2026-01-08 15:36:35 -0500

    a

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/IntakeTestNextFTC.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/IntakeTestNextFTC.java
@@ -2,10 +2,13 @@ package org.firstinspires.ftc.teamcode.testing;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.subsystems.Intake;
+
+import dev.nextftc.bindings.BindingManager;
 import dev.nextftc.core.commands.Command;
 import dev.nextftc.core.components.BindingsComponent;
 import dev.nextftc.core.components.SubsystemComponent;
 import dev.nextftc.ftc.Gamepads;
+import dev.nextftc.bindings.Bindings.*;
 import dev.nextftc.ftc.NextFTCOpMode;
 import dev.nextftc.ftc.components.BulkReadComponent;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
@@ -24,19 +27,20 @@ public class IntakeTestNextFTC extends NextFTCOpMode {
     public void onStartButtonPressed() {
         //left intake control
 
-        Gamepads.gamepad1().dpadUp()
-                .whenBecomesTrue(Intake.INSTANCE.increaseLeft());
+        Gamepads.gamepad1().dpadUp().whenTrue(Intake.INSTANCE.increaseLeft);
         Gamepads.gamepad1().dpadDown()
-                .whenBecomesTrue(Intake.INSTANCE.decreaseLeft());
+                .whenTrue(Intake.INSTANCE.decreaseLeft);
         Gamepads.gamepad1().x()
-                        .whenBecomesTrue(Intake.INSTANCE.increaseRight());
-        Gamepads.gamepad1().a().whenBecomesTrue(Intake.INSTANCE.decreaseRight());
+                        .whenTrue(Intake.INSTANCE.increaseRight());
+        Gamepads.gamepad1().a().whenTrue(Intake.INSTANCE.decreaseRight());
 
     }
     @Override
     public void onUpdate() {
+        BindingManager.update();
         telemetry.addData("Left Intake Power", Intake.INSTANCE.getLeftPower());
         telemetry.addData("Right Intake Power", Intake.INSTANCE.getRightPower());
         telemetry.update();
+
     }
 }
