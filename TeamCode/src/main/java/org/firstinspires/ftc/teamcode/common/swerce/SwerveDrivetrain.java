3697d61afd040e52dfd2bd95cbc009c2dceae333
Pirate1113 <156390222+Pirate1113@users.noreply.github.com>
2026-01-09 20:43:25 -0500

    change

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/swerce/SwerveDrivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/swerce/SwerveDrivetrain.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.swerveDrive;
+package org.firstinspires.ftc.teamcode.common.swerce;
 
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.IMU;
