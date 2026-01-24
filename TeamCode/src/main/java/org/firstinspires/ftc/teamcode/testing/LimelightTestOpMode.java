a1f75f9cbf1499cf4f4979db07ae4fd73c212a27
yarilightcav <71334766+badgamer2007@users.noreply.github.com>
2026-01-24 12:58:33 -0500

    commented out unworking and

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/LimelightTestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/LimelightTestOpMode.java
@@ -1,36 +1,36 @@
-package org.firstinspires.ftc.teamcode.testing;
-
-import com.qualcomm.hardware.limelightvision.Limelight3A;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
-@TeleOp(name = "Limelight Test", group = "Testing")
-public class LimelightTestOpMode extends LinearOpMode {
-
-    @Override
-    public void runOpMode() {
-
-        Limelight3A limelight = hardwareMap.get(Limelight3A.class, "limelight");
-
-        limelight.start();
-
-        LimelightAngle vision = new LimelightAngle(
-                limelight,
-                6.0,    // shooter height
-                12.5    // tag offset
-        );
-
-        telemetry.addLine("Limelight initialized");
-        telemetry.update();
-
-        waitForStart();
-
-        while (opModeIsActive()) {
-            telemetry.addData("Has Target", vision.hasTarget());
-            telemetry.addData("Yaw (tx)", vision.getYaw());
-            telemetry.addData("Pitch (ty)", vision.getPitch());
-            telemetry.addData("Distance (in)", vision.getDistance());
-            telemetry.update();
-        }
-    }
-}
+//package org.firstinspires.ftc.teamcode.testing;
+//
+//import com.qualcomm.hardware.limelightvision.Limelight3A;
+//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+//import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+//
+//@TeleOp(name = "Limelight Test", group = "Testing")
+//public class LimelightTestOpMode extends LinearOpMode {
+//
+//    @Override
+//    public void runOpMode() {
+//
+//        Limelight3A limelight = hardwareMap.get(Limelight3A.class, "limelight");
+//
+//        limelight.start();
+//
+//        LimelightAngle vision = new LimelightAngle(
+//                limelight,
+//                6.0,    // shooter height
+//                12.5    // tag offset
+//        );
+//
+//        telemetry.addLine("Limelight initialized");
+//        telemetry.update();
+//
+//        waitForStart();
+//
+//        while (opModeIsActive()) {
+//            telemetry.addData("Has Target", vision.hasTarget());
+//            telemetry.addData("Yaw (tx)", vision.getYaw());
+//            telemetry.addData("Pitch (ty)", vision.getPitch());
+//            telemetry.addData("Distance (in)", vision.getDistance());
+//            telemetry.update();
+//        }
+//    }
+//}
