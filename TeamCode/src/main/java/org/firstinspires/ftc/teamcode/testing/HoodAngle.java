94c1c7dd32a0e94286845c0af2bbdfc0b62e076b
adityatangella <adityatangella9@gmail.com>
2026-01-26 22:20:04 -0500

    pushpush

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/HoodAngle.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/HoodAngle.java
@@ -74,7 +74,7 @@ public class HoodAngle {
         double c = y + (g*Math.pow(x, 2))/(2*Math.pow(v0, 2));
 
         double discriminant = b*b - 4*a*c;
-        double tanTheta = (-b - Math.sqrt(discriminant)) / (2*a);
+        double tanTheta = (-b + Math.sqrt(discriminant)) / (2*a);
         // make the sign in front of the discriminant a positive for the high angle
         double projectileAngleRad = Math.atan(tanTheta);
         double angleDeg = Math.toDegrees(projectileAngleRad);
