006f2590409916914eac95ade1576497b01bb65a
yarilightcav <71334766+badgamer2007@users.noreply.github.com>
2026-02-01 10:28:18 -0500

    i trird ftc dashbord

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/swerce/SwerveTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/swerce/SwerveTeleOp.java
@@ -1,6 +1,9 @@
 package org.firstinspires.ftc.teamcode.common.swerce;
 
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 import dev.nextftc.core.components.BindingsComponent;
 import dev.nextftc.core.components.SubsystemComponent;
@@ -9,6 +12,10 @@ import dev.nextftc.ftc.components.BulkReadComponent;
 
 @TeleOp(name = "Swerve TeleOp")
 public class SwerveTeleOp extends NextFTCOpMode {
+
+    Telemetry dashboardTelemetry;
+    FtcDashboard dashboard;
+
 
     public SwerveTeleOp() {
         addComponents(
