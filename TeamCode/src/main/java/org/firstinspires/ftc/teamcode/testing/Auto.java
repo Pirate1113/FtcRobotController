e4bcbec7144254b9be5e25cf505d971b186583e4
adityatangella <adityatangella9@gmail.com>
2026-03-15 08:37:36 -0400

    autoauto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/Auto.java
@@ -1,61 +1,61 @@
-//package org.firstinspires.ftc.teamcode.testing;
-//
-//import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-//
-//import dev.nextftc.ftc.NextFTCOpMode;
-//import dev.nextftc.core.components.SubsystemComponent;
-//
-//import org.firstinspires.ftc.teamcode.common.swerce.SwerveDrivetrain;
-//import org.firstinspires.ftc.teamcode.subsystems.Turret;
-//import org.firstinspires.ftc.teamcode.testing.LimelightAngle;
-//
-//@Autonomous(name = "Forward + Limelight Aim")
-//public class Auto extends NextFTCOpMode {
-//
-//    private LimelightAngle limelight;
-//
-//    public Auto () {
-//        addComponents(
-//                new SubsystemComponent(SwerveDrivetrain.INSTANCE),
-//                new SubsystemComponent(Turret.INSTANCE)
-//        );
-//    }
-//
-//    @Override
-//    public void onInit() {
-//
-//        limelight = new LimelightAngle(
-//                hardwareMap,
-//                "limelight",
-//                10,
-//                60
-//        );
-//
-//        limelight.pipelineSwitch(0);
-//    }
-//
-//    @Override
-//    public void onStartButtonPressed() {
-//
-//        try {
-//
-//            // move forward a short distance
-//            SwerveDrivetrain.INSTANCE.autoDrive(0.4);
-//            Thread.sleep(1200);
-//            SwerveDrivetrain.INSTANCE.stop();
-//
-//            // aim using limelight?? this needs work?
-//            if (limelight.hasTarget()) {
-//
-//                double yaw = limelight.getYaw();
-//
-//                // turn turret to angle
-//                Turret.INSTANCE.setTargetAngle(yaw);
-//
-//            }
-//
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//}
+package org.firstinspires.ftc.teamcode.testing;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import dev.nextftc.ftc.NextFTCOpMode;
+import dev.nextftc.core.components.SubsystemComponent;
+
+import org.firstinspires.ftc.teamcode.common.swerce.SwerveDrivetrain;
+import org.firstinspires.ftc.teamcode.subsystems.Turret;
+import org.firstinspires.ftc.teamcode.testing.LimelightAngle;
+
+@Autonomous(name = "Forward + Limelight Aim")
+public class Auto extends NextFTCOpMode {
+
+    private LimelightAngle limelight;
+
+    public Auto () {
+        addComponents(
+                new SubsystemComponent(SwerveDrivetrain.INSTANCE),
+                new SubsystemComponent(Turret.INSTANCE)
+        );
+    }
+
+    @Override
+    public void onInit() {
+
+        limelight = new LimelightAngle(
+                hardwareMap,
+                "limelight",
+                10,
+                60
+        );
+
+        limelight.pipelineSwitch(0);
+    }
+
+    @Override
+    public void onStartButtonPressed() {
+
+        try {
+
+            // move forward a short distance
+            SwerveDrivetrain.INSTANCE.autoDrive(0.4);
+            Thread.sleep(1200);
+            SwerveDrivetrain.INSTANCE.stop();
+
+            // aim using limelight?? this needs work?
+            if (limelight.hasTarget()) {
+
+                double yaw = limelight.getYaw();
+
+                // turn turret to angle
+                Turret.INSTANCE.setBaseAngle(yaw);
+
+            }
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
